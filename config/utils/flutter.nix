6113ac75c2fe06c067eb90d14f8af7bc946f943f
DreamingCodes <me@dreaming.codes>
2024-12-04 23:42:28 +0100

    fix flutter tools init

--- config/utils/flutter.nix
+++ config/utils/flutter.nix
@@ -2,8 +2,8 @@
   options = { flutter.enable = lib.mkEnableOption "Enable flutter module"; };
   config = lib.mkIf config.flutter.enable {
     extraPlugins = with pkgs.vimPlugins; [ flutter-tools-nvim dart-vim-plugin ];
+    extraConfigLua = ''
+      require("flutter-tools").setup {}
+    '';
   };
-  extraConfigLua = ''
-    require("flutter-tools").setup {}
-  '';
 }
